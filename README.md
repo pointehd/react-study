339ffd22f1a40b643c25122825078fe8d9742d6b
Donghyun Hwang <pointehd@naver.com>
2020-12-07 21:15:03 +0900

    4. JSX 완료
    https://react.vlpt.us/basic/04-jsx.html

--- README.md
+++ README.md
@@ -7,7 +7,10 @@
 
 
 ## 개발기록
-> 20.12.06 : https://react.vlpt.us/basic/02-prepare.html 페이지 따라가기 벨로퍼트님 강의 따라가기!    
+* 20.12.06 : 아주 처음보는것같은데 아자! 
+> 4. JSX 완료
+
+* 20.12.06 : https://react.vlpt.us/basic/02-prepare.html 페이지 따라가기 벨로퍼트님 강의 따라가기!    
 
 ```
 $ npx create-react-app begin-react
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,20 @@
 import react from 'react';
+import './App.css'
 
 function Hello(){
-    return <div>hellow react</div>
+    const name = 'test';
+    const style = {
+        backgroundColor : 'black',
+        color: 'white'
+    };
+    return (
+        <>
+            <div>hellow react</div>
+            <div className='class-test' style={style}>{name}</div>
+
+            {/*javascript 주석 */}
+        </>
+    );
 }
 
 export default Hello;

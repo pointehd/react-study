bd222ea21cc0a3c6117c98e6aa3a6e4ec6d527c2
Donghyun Hwang <pointehd@naver.com>
2020-12-10 22:33:08 +0900

    8. input 상태 관리하기
    https://react.vlpt.us/basic/08-manage-input.html

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import react from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Countrer';
-
+import InputSample from './InputSample'
 function App() {
   return (
     <Wrapper>
+      <InputSample/>
       <Counter/>
       <Hello name="hello react" color="red"/>
       <Hello name="hello react" color="red" isSpecial/>

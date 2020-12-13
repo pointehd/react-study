9d67350b932d9b5587163405b870abb0982cc0eb
Donghyun Hwang <pointehd@naver.com>
2020-12-13 23:40:07 +0900

    ~20. useReducer 를 사용하여 상태 업데이트 로직 분리하기 완료

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useMemo, useCallback} from 'react';
+import React, {useRef, useReducer, useMemo, useCallback} from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Countrer';
@@ -10,25 +10,12 @@ function countActiveUsers(users){
   console.log('활성 사용자 수를 세는중...');
   return users.filter(user => user.active).length;
 }
-
-function App() {
-  const [inputs, setInputs] = useState({
-    userName : '',
+const initialState = {
+  inputs: {
+    userName :'',
     email : ''
-  });
-
-  const {userName, email} = inputs;
-  const onChange = useCallback(
-    e =>{
-      const {name, value} = e.target;
-      setInputs({
-        ...inputs,
-        [name] : value
-      });
-    },
-    [inputs]
-  );
-  const [users, setUsers] = useState([
+  },
+  users : [
     {
       id:1,
       username:'donghyun',
@@ -47,67 +34,107 @@ function App() {
         email:'test@gmail.com',
         active: false
     }
-  ]);
+  ]
+};
+
+function reducer(state, action){
+  switch(action.type){
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs : {
+          ...state.inputs,
+          [action.name] : action.value
+        }
+      };
+
+    case 'CREATE_USER':
+      // user : state.users.concat(action.user);
+      // equals
+      // user : [...state.users, users];
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { users } = state;
+  const { userName, email } = state.inputs;
   const nextId = useRef(4);
-  const onRemove = useCallback(
-    id =>{
-      setUsers(users.filter(user =>user.id !== id));
-    },
-    [users]
+  const onChange = useCallback(
+    e => {
+      const {name, value} = e.target;
+      dispatch({
+        type:'CHANGE_INPUT',
+        name,
+        value
+      });
+    },[]
   );
-  const onCreate = useCallback(
-    () =>{
-      const user = {
-        id:nextId.current,
+  const onCreate = useCallback(() => {
+    dispatch({
+      type:'CREATE_USER',
+      user:{
+        id: nextId.current,
         username : userName,
         email
-      };
-      // spread 연산자 
-      setUsers([...users, user]);
-      /**
-       * 둘중 하나사용
-       * setUser(user.concat(user));
-       * 
-       */
-  
-      setInputs({
-        userName:'',
-        email:''
-      });
-      nextId.current += 1;
-    },
-    [users,userName, email]
-  );
-  const onToggle = useCallback(
-    (id) =>{
-      setUsers(
-        users.map(user =>
-            user.id === id ? {...user, active:!user.active} : user
-          )
-      );
-    },
-    [users]
-  );
-  const count = useMemo(() => countActiveUsers(users), [users]); ;
+      }
+    });
+    nextId.current +=1;
+  },[userName, email]);
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, []);
+
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
-    <Wrapper>
+    <>
       <CreateUser 
         userName={userName}
         email={email}
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
-      <div>활성 사용자 수 : {count}</div>
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
+      <div>활성 사용자 수 : 0</div>
+      {/**
+       * 
       <InputSample/>
       <Counter/>
       <Hello name="hello react" color="red"/>
       <Hello name="hello react" color="red" isSpecial/>
+       */}
       {/** 
        * isSpecial={ture}  isSpecial 과 같음  
        * 신기하넹..
       */}
-    </Wrapper>
+    </>
   );
 }
 
